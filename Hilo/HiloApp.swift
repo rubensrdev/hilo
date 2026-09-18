9d26e36123dd289f6e036ae40e9338b6bfa37480
Rubén Segura <ruben.sr.apple@gmail.com>
2026-09-18 12:04:45 +0200

    Initial Commit

---
+++ Hilo/HiloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiloApp.swift
+//  Hilo
+//
+//  Created by Rubén Segura Romo on 18/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct HiloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
